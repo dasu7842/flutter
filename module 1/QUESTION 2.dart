77c68dd94da131842a546cd2edf7227ca2c65a6f
dasu7842 <140416425+dasu7842@users.noreply.github.com>
2023-12-28 17:02:54 +0530

    28-12-2023

---
+++ module 1/QUESTION 2.dart
@@ -0,0 +1,12 @@
+// Write a program to make a square and cube of number.
+
+int main()
+{
+  int a,b,c;
+  a=10;
+  c=a*a;
+  b=a*a*a;
+  print("square number is:$c");
+  print("cube number is:$b");
+  return 0;
+}
